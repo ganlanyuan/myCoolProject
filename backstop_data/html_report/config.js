30d3aa0030972e487602a81cff8ff090d8864667
William Lin 22 <ganlanyuan@users.noreply.github.com>
2017-10-09 16:24:38 -0400

    add backstopjs local test

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,257 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_0_navbar_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_0_navbar_0_phone.png",
+        "selector": ".navbar",
+        "fileName": "test_My_Homepage_0_navbar_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_1_jumbotron_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_1_jumbotron_0_phone.png",
+        "selector": ".jumbotron",
+        "fileName": "test_My_Homepage_1_jumbotron_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_2_bodycol-md-4nth-of-type1_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_2_bodycol-md-4nth-of-type1_0_phone.png",
+        "selector": "body .col-md-4:nth-of-type(1)",
+        "fileName": "test_My_Homepage_2_bodycol-md-4nth-of-type1_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_3_bodycol-md-4nth-of-type2_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_3_bodycol-md-4nth-of-type2_0_phone.png",
+        "selector": "body .col-md-4:nth-of-type(2)",
+        "fileName": "test_My_Homepage_3_bodycol-md-4nth-of-type2_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_4_bodycol-md-4nth-of-type3_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_4_bodycol-md-4nth-of-type3_0_phone.png",
+        "selector": "body .col-md-4:nth-of-type(3)",
+        "fileName": "test_My_Homepage_4_bodycol-md-4nth-of-type3_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_5_footer_0_phone.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_5_footer_0_phone.png",
+        "selector": "footer",
+        "fileName": "test_My_Homepage_5_footer_0_phone.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_0_navbar_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_0_navbar_1_tablet.png",
+        "selector": ".navbar",
+        "fileName": "test_My_Homepage_0_navbar_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_1_jumbotron_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_1_jumbotron_1_tablet.png",
+        "selector": ".jumbotron",
+        "fileName": "test_My_Homepage_1_jumbotron_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_2_bodycol-md-4nth-of-type1_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_2_bodycol-md-4nth-of-type1_1_tablet.png",
+        "selector": "body .col-md-4:nth-of-type(1)",
+        "fileName": "test_My_Homepage_2_bodycol-md-4nth-of-type1_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_3_bodycol-md-4nth-of-type2_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_3_bodycol-md-4nth-of-type2_1_tablet.png",
+        "selector": "body .col-md-4:nth-of-type(2)",
+        "fileName": "test_My_Homepage_3_bodycol-md-4nth-of-type2_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_4_bodycol-md-4nth-of-type3_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_4_bodycol-md-4nth-of-type3_1_tablet.png",
+        "selector": "body .col-md-4:nth-of-type(3)",
+        "fileName": "test_My_Homepage_4_bodycol-md-4nth-of-type3_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_My_Homepage_5_footer_1_tablet.png",
+        "test": "../bitmaps_test/20171009-155803/test_My_Homepage_5_footer_1_tablet.png",
+        "selector": "footer",
+        "fileName": "test_My_Homepage_5_footer_1_tablet.png",
+        "label": "My Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const backstopjs = require('backstopjs');
+
+gulp.task('ref', () => backstopjs('reference'));
+gulp.task('test', () => backstopjs('test'));
+
+
+// https://github.com/garris/BackstopJS
+
+// 1. cd project/root/
+// 2. npm install -g backstopjs
+// 3. backstop init
+// 4. update backstop.json
+// 5. backstop reference
+// 6. backstop test
+// 7. backstop approve
